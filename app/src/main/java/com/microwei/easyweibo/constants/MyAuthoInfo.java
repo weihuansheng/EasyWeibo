379173da905cc53cda58b2aea9ce7d50ef9e107e
weihuansheng <962541537@qq.com>
2016-11-16 20:02:35 +0800

    change

--- app/src/main/java/com/microwei/easyweibo/constants/MyAuthoInfo.java
+++ app/src/main/java/com/microwei/easyweibo/constants/MyAuthoInfo.java
@@ -2,9 +2,6 @@ package com.microwei.easyweibo.constants;
 
 import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 
-/**
- * Created by Administrator on 2016-04-03 .
- */
 public class MyAuthoInfo {
     public static final String APP_KEY = "534560909";
     public static final String REDIRECT_URL = "https://api.weibo.com/oauth2/default.html";
